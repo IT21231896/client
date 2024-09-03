7833c815b6a6eb7314a9759a5f95fddae08e3036
kijan <mohamedkijan321@gmail.com>
2024-09-03 09:45:46 +0530

    comm

--- src/Pages/ChangePassword.js
+++ src/Pages/ChangePassword.js
@@ -18,39 +18,7 @@ function ChangePassword() {
             <Sidebar />
             <div className="change-password-content">
                 <h2>Change Password</h2>
-                <form onSubmit={handlePasswordChange}>
-                    <div className="form-group">
-                        <label htmlFor="current-password">Current Password</label>
-                        <input
-                            type="password"
-                            id="current-password"
-                            value={currentPassword}
-                            onChange={(e) => setCurrentPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="new-password">New Password</label>
-                        <input
-                            type="password"
-                            id="new-password"
-                            value={newPassword}
-                            onChange={(e) => setNewPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="confirm-password">Confirm Password</label>
-                        <input
-                            type="password"
-                            id="confirm-password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <button type="submit" className="save-button">Save</button>
-                </form>
+
             </div>
         </div>
     );
